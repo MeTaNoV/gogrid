27928849232e7c74c40cabb31f288b0b8d603908
pascalg <pascalg@dots.de>
2015-06-29 18:08:26 +0200

    structure revamp

---
+++ gogrid.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/MeTaNoV/griddler"
+	"os"
+)
+
+var fileName string
+
+func initFlags() {
+	const (
+		defaultFilename = "default.grid"
+		usageFilename   = "name of the griddler file to load."
+	)
+	flag.StringVar(&fileName, "file", defaultFilename, usageFilename)
+	flag.StringVar(&fileName, "f", defaultFilename, usageFilename)
+
+	flag.Parse()
+
+	if fileName == defaultFilename {
+		flag.Usage()
+		os.Exit(1)
+	}
+}
+
+func main() {
+	initFlags()
+
+	gBoard := griddler.New()
+	err := gBoard.Load(fileName)
+	if err != nil {
+		fmt.Printf("Error loading file: %v\n", err)
+		return
+	}
+
+	isSolved := gBoard.Solve()
+	if isSolved {
+		fmt.Println("Griddler completed!!!")
+	} else {
+		fmt.Println("Griddler uncompleted, find new search algorithm!")
+	}
+	//gBoard.Show()
+}
